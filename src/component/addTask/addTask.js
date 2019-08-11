81a2aadef9adc9061c9adaf9c64a3ee4892ebcbd
gxqn <627341160@qq.com>
2019-08-11 21:54:22 +0800

    enter键添加新任务

--- src/component/addTask/addTask.js
+++ src/component/addTask/addTask.js
@@ -5,6 +5,7 @@ class AddInput extends Component {
   constructor(props) {
     super(props)
 		this.handleAddBtnClick = this.handleAddBtnClick.bind(this)
+		this.handleKeyUp = this.handleKeyUp.bind(this)
   }
 
 	handleAddBtnClick() {
@@ -19,11 +20,17 @@ class AddInput extends Component {
 		}
 	}
 
+	handleKeyUp(e) {
+		if(e.keyCode == 13) {
+			this.handleAddBtnClick()
+		}
+	}
+
   render() {
     return (
       <div className="addWrap">
 				<span className="addTitle">新建任务：</span>
-				<input className="addIpt" ref="ipt" placeholder="请输入新的任务"></input>
+				<input className="addIpt" ref="ipt" placeholder="请输入新的任务" onKeyUp={this.handleKeyUp}></input>
 				<button className="addBtn" onClick={this.handleAddBtnClick}>保存</button>
       </div>
     )
